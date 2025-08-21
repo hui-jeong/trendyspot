fde12aa25c316d577c83b880906e0069b88ce24d
hui-jeong <huijeong1028@gmail.com>
2025-08-21 14:57:03 +0900

    chore: initial commit

---
+++ src/main/java/com/study/TrendySpot/TrendySpotApplication.java
@@ -0,0 +1,13 @@
+package com.study.TrendySpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendySpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendySpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/TrendySpot/TrendySpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.TrendySpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendySpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
