678ac3b289a2879da8ab1b04cf3f9740885daea4
hui-jeong <huijeong1028@gmail.com>
2025-09-11 23:51:20 +0900

    feat: 리뷰 응답 DTO 정리

--- src/main/java/com/study/trendyspot/dto/ReviewItemDto.java
+++ src/main/java/com/study/trendyspot/dto/ReviewItemDto.java
@@ -1,10 +1,10 @@
 package com.study.trendyspot.dto;
 
 public record ReviewItemDto(
-        String type, // 블로그인지 뉴스인지
+        ReviewType type,
         String title,
-        String contents, //내용
-        String sourceName, //출처
+        String contents,
+        String sourceName,
         String link
 ) {
 }
